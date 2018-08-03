43054d2656e439513e15754d6d254c4d629120d1
Ben Jason <benjason777@gmail.com>
2018-08-03 01:21:46 -0400

    send signup confirmation email when a user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_signup_confirmation_email
+
+
+  private
+  def send_signup_confirmation_email
+  	UserNotifier.send_signup_email(self).deliver
+  end
+
 end
