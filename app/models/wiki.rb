55f50e9357daf18003a44a6dd5c4d448c7bddfeb
Ben Jason <benjason777@gmail.com>
2018-09-05 22:16:30 -0400

    add association between wikis and collaborators

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,10 +1,12 @@
 class Wiki < ApplicationRecord
   belongs_to :user
+  has_many :collaborations
+  has_many :collaborators, through: :collaborations, source: :user
 
-    after_initialize do |wiki|
-      if wiki.private.nil?
-        wiki.private = false
-      end
+  after_initialize do |wiki|
+    if wiki.private.nil?
+      wiki.private = false
     end
+  end
 
 end
